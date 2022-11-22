8212e8ccb92d480b9332ec7be45f823150b63dd6
michael haroon <hmikehecht@gmail.com>
2022-11-21 18:58:47 -0800

    added pseudocode to Engine.java

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -3,16 +3,23 @@ package byow.Core;
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
 
+import java.util.ArrayList;
+
 public class Engine {
     TERenderer ter = new TERenderer();
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
+    private boolean gameOver;
 
     /**
      * Method used for exploring a fresh world. This method should handle all inputs,
      * including inputs from the main menu.
      */
+
+    public Engine() {
+        gameOver = false;
+    }
     public void interactWithKeyboard() {
     }
 
@@ -46,7 +53,66 @@ public class Engine {
         // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
         // that works for many different input types.
 
-        TETile[][] finalWorldFrame = null;
-        return finalWorldFrame;
+//        TETile[][] finalWorldFrame = null;
+//        return finalWorldFrame;
+
+        /* Suppose real game starting from menu. */
+        /* The player either loads, starts, or quits
+                for (int i = 0; i < input.length; i++) {
+                    if (input[i].toUpperCase() == 'N') {
+                        createNewGame(input.substring(i:));
+                    }
+                    if (input[i].upperCase() == 'L') {
+                        loadPreviousGame(input.substring(i:));
+                    }
+                    if (input[i].toUpperCase() == 'Q') {
+                        quit();
+                    }
+                }
+        *
+        * */
+
     }
+
+    /* private _____ createNewGame(String restOfInput) {
+        String seed = "" ;
+        int index = 0;
+        while (input[index] is an int) {
+            seed += input[index];
+        }
+    *   generateWorld(Long.parseLong(seed));
+    *   generateHUD();
+    *   startGame();
+    * } */
+
+    /* private ____ generateWorld(long seed) {
+    *   // does what testMain does to generate world randomly
+    *       // testMain should also generate an avatar and door
+    * }*/
+
+    /* private ____ generateHUD() {
+    *   // uses Std.draw and instance variables to create and update HUD
+    * } */
+
+    /*
+    private void startGame(String input) {
+        int index = 0;
+           while (!gameOver) {
+            drawFrame();
+            if (input[index] == ':' $$ input[index+1] == 'Q') {
+                quit();
+            }
+            if (input[index].toUppercase == 'W') {
+                moveUp();
+            if (input[index].toUppercase == 'A') {
+                moveLeft();
+           }
+           if (input[index].toUppercase == 'S') {
+                moveDown();
+           }
+           if (input[index].toUppercase == 'D') {
+                moveRight();
+           }
+    }
+     */
 }
