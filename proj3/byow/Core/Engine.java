0723494d5c807fce349c35e544ae38ef1edae941
michael haroon <hmikehecht@gmail.com>
2022-11-22 16:58:29 -0800

    added TestMain.java

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -12,15 +12,85 @@ public class Engine {
     public static final int HEIGHT = 30;
     private boolean gameOver;
 
+    public Engine() {
+        gameOver = false;
+    }
+
     /**
      * Method used for exploring a fresh world. This method should handle all inputs,
      * including inputs from the main menu.
      */
 
-    public Engine() {
-        gameOver = false;
-    }
     public void interactWithKeyboard() {
+        /* Suppose real game starting from menu. */
+        /* The player either loads, starts, or quits
+                for (int i = 0; i < input.length; i++) {
+                    if (input[i].toUpperCase() == 'N') {
+                        createNewGame(input.substring(i:));
+                    }
+                    if (input[i].upperCase() == 'L') {
+                        loadPreviousGame(input.substring(i:));
+                    }
+                    if (input[i].toUpperCase() == 'Q') {
+                        quit();
+                    }
+                }
+        *
+        * */
+
+    }
+
+    /* private _____ createNewGame(String restOfInput) {
+        String seed = "" ;
+        String playerInput = "";
+        int index = 0;
+        while (input[index] is an int) {
+            seed += input[index];
+        }
+        for (int i = 0; i < input.length; i++) {
+            if (input[i].toUpperCase() == 'Q') {
+                if (input[i-1] == ':') {
+                    make txt file
+                    save input in it
+                }
+                quit(); //kill program
+            }
+            playerInput
+        }
+    *   generateWorld(Long.parseLong(seed),);
+    *   generateHUD();
+    *   startGame();
+    * } */
+
+    /* private ____ generateWorld(long seed) {
+     *   // does what testMain does to generate world randomly
+     *       // testMain should also generate an avatar and door
+     * }*/
+
+    /* private ____ generateHUD() {
+     *   // uses Std.draw and instance variables to create and update HUD
+     * } */
+
+    /*
+    private void startGame(String input) {
+        int index = 0;
+           while (!gameOver) {
+            drawFrame();
+            if (input[index] == ':' $$ input[index+1] == 'Q') {
+                quit();
+            }
+            if (input[index].toUppercase == 'W') {
+                moveUp();
+            if (input[index].toUppercase == 'A') {
+                moveLeft();
+           }
+           if (input[index].toUppercase == 'S') {
+                moveDown();
+           }
+           if (input[index].toUppercase == 'D') {
+                moveRight();
+           }
+    }
     }
 
     /**
@@ -76,11 +146,22 @@ public class Engine {
 
     /* private _____ createNewGame(String restOfInput) {
         String seed = "" ;
+        String playerInput = "";
         int index = 0;
         while (input[index] is an int) {
             seed += input[index];
         }
-    *   generateWorld(Long.parseLong(seed));
+        for (int i = 0; i < input.length; i++) {
+            if (input[i].toUpperCase() == 'Q') {
+                if (input[i-1] == ':') {
+                    make txt file
+                    save input in it
+                }
+                quit(); //kill program
+            }
+            playerInput
+        }
+    *   generateWorld(Long.parseLong(seed),);
     *   generateHUD();
     *   startGame();
     * } */
@@ -114,5 +195,9 @@ public class Engine {
                 moveRight();
            }
     }
+
+                            ANOTHER SOLUTION
+          read through user input, change coordinate of avatar before loading world,
+          then load world with new location.
      */
 }
---
+++ proj3/byow/Core/TestMain.java
@@ -0,0 +1,342 @@
+package byow.Core;
+
+import byow.TileEngine.TERenderer;
+import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+
+import java.util.Random;
+
+/** This is the main entry point for the program. This class simply parses
+ *  the command line inputs, and lets the byow.Core.Engine class take over
+ *  in either keyboard or input string mode.
+ */
+public class TestMain {
+    static final int seed = 7; //just a random seed for testin'
+    static final int w = 70; //j a decent width size
+    static final int h = 35; //j a decent height size
+    static final String[] directions = new String[]{"up", "down", "left", "right"}; //directions
+
+    static final Random random = new Random(seed);
+
+    static final TETile[][] world = new TETile[w][h];
+
+    public static void main(String[] args) {
+
+        TERenderer ter = new TERenderer();
+        ter.initialize(w, h);
+
+        //make evryting outside initially
+        for (int x = 0; x < w; x += 1) {
+            for (int y = 0; y < 35; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        //find us a random tile
+        int randomX = RandomUtils.uniform(random, w);
+        int randomY = RandomUtils.uniform(random, h);
+
+        //pick a random direction
+        String randomD1 = directions[RandomUtils.uniform(random, 4)];
+        String randomD2 = directions[RandomUtils.uniform(random, 4)];
+
+        //now choose random lengths lol
+        int randomD1L = RandomUtils.uniform(random, w/10);
+        int randomD2L = RandomUtils.uniform(random, h/10);
+
+        System.out.println("X: " + randomX + " Y: " + randomY + " D1: " + randomD1 + " D2: " + randomD2 + " D1L: " + randomD1L + " D2L: " + randomD2L);
+
+        int[] firstpop = populate(randomX, randomY, randomD1, randomD2, randomD1L, randomD2L);
+
+        int nowX = firstpop[0];
+
+        System.out.println(nowX);
+
+        int nowY = firstpop[1];
+
+        System.out.println(nowY);
+
+        for(int i = 0; i < 350; i++) {
+            String nowD1 = directions[RandomUtils.uniform(random, 4)];
+            String nowD2 = directions[RandomUtils.uniform(random, 4)];
+
+            int nowD1L = RandomUtils.uniform(random, w/10);
+            int nowD2L = RandomUtils.uniform(random, h/10);
+
+            System.out.println("X: " + nowX + " Y: " + nowY + " D1: " + nowD1 + " D2: " + nowD2 + " D1L: " + nowD1L + " D2L: " + nowD2L);
+
+            int[] secpop = populate(nowX, nowY, nowD1, nowD2, nowD1L, nowD2L);
+
+            nowX = secpop[0];
+            System.out.println(nowX);
+
+            nowY = secpop[1];
+            System.out.println(nowY);
+        }
+
+        /**for (int x = 1; x < w - 1; x += 1) {
+         if (world[x][0] == Tileset.INSIDE) {
+         world[--x][0] = Tileset.WALL;
+         world[++x][0] = Tileset.WALL;
+         world[++x][0] = Tileset.WALL;
+         }
+         }
+         for (int x = 1; x < w - 1; x += 1) {
+         if (world[x][h-1] == Tileset.INSIDE) {
+         world[--x][0] = Tileset.WALL;
+         world[++x][0] = Tileset.WALL;
+         world[++x][0] = Tileset.WALL;
+         }
+         }
+         for (int y = 1; y < h-1; y++) {
+         if (world[0][y] == Tileset.INSIDE) {
+         world[--y][0] = Tileset.WALL;
+         world[++y][0] = Tileset.WALL;
+         world[++y][0] = Tileset.WALL;
+         }
+         }
+         for (int y = 1; y < h-1; y++) {
+         if (world[w-1][y] == Tileset.INSIDE) {
+         world[--y][0] = Tileset.WALL;
+         world[++y][0] = Tileset.WALL;
+         world[++y][0] = Tileset.WALL;
+         }
+         }**/
+
+        for (int x = 1; x < w-1; x++) {
+            for (int y = 1; y < h-1; y++) {
+                if(world[x][y] == Tileset.OUTSIDE) {
+                    if(world[x+1][y] == Tileset.INSIDE || world[x-1][y] == Tileset.INSIDE
+                            || world[x][y+1] == Tileset.INSIDE || world[x][y-1] == Tileset.INSIDE
+                            || world[x+1][y+1] == Tileset.INSIDE || world[x-1][y-1] == Tileset.INSIDE
+                            || world[x-1][y+1] == Tileset.INSIDE || world[x+1][y-1] == Tileset.INSIDE) {
+                        world[x][y] = Tileset.WALL;
+                    }
+                }
+            }
+        }
+
+        for (int x = 1; x < w - 1; x++) {
+            if(world[x][1] == Tileset.INSIDE || world[x+1][1] == Tileset.INSIDE || world[x-1][1] == Tileset.INSIDE) {
+                world[x][0] = Tileset.WALL;
+            }
+        }
+
+        for (int x = 1; x < w - 1; x++) {
+            if(world[x][h-2] == Tileset.INSIDE || world[x+1][h-2] == Tileset.INSIDE || world[x-1][h-2] == Tileset.INSIDE) {
+                world[x][h-1] = Tileset.WALL;
+            }
+        }
+
+        for (int y = 1; y < h - 1; y++) {
+            if(world[1][y] == Tileset.INSIDE || world[1][y+1] == Tileset.INSIDE || world[1][y-1] == Tileset.INSIDE) {
+                world[0][y] = Tileset.WALL;
+            }
+        }
+
+        for (int y = 1; y < h - 1; y++) {
+            if(world[w-2][y] == Tileset.INSIDE || world[w-2][y+1] == Tileset.INSIDE || world[w-2][y-1] == Tileset.INSIDE) {
+                world[w-1][y] = Tileset.WALL;
+            }
+        }
+
+        ter.renderFrame(world);
+    }
+
+    private static int[] populate(int X, int Y, String D1, String D2, int D1L, int D2L) {
+        int initialX = X;
+        int initialY = Y;
+
+        world[X][Y] = Tileset.INSIDE;
+
+        switch (D1) {
+            case "right" : {
+                for (int i = 0; i < D1L; i++) {
+                    if (reBound(X, Y) != 0) {
+                        initialX = reBound(X, Y);
+                        X = reBound(X, Y);
+                    }
+                    world[++X][Y] = Tileset.INSIDE;
+
+                    System.out.println("D1 Right-- X position:" + X);
+                }
+                break;
+            }
+            case "left" : {
+                for (int i = 0; i < D1L; i++) {
+                    if (reBound(X, Y) != 0) {
+                        initialX = reBound(X, Y);
+                        X = reBound(X, Y);
+                    }
+                    world[--X][Y] = Tileset.INSIDE;
+
+                    System.out.println("D1 Left-- X position:" + X);
+                }
+                break;
+            }
+            case "up" : {
+                for (int i = 0; i < D1L; i++) {
+                    if (reBound(X, Y) != 0) {
+                        initialY = reBound(X, Y);
+                        Y = reBound(X, Y);
+                    }
+                    world[X][++Y] = Tileset.INSIDE;
+
+                    System.out.println("D1 Up-- Y position:" + Y);
+                }
+                break;
+            }
+            case "down" : {
+                for (int i = 0; i < D1L; i++) {
+                    if (reBound(X, Y) != 0) {
+                        initialY = reBound(X, Y);
+                        Y = reBound(X, Y);
+                    }
+                    world[X][--Y] = Tileset.INSIDE;
+
+                    System.out.println("D1 Down-- Y position:" + Y);
+                }
+                break;
+            }
+        }
+
+        switch(D2) {
+            case "right" : {
+                for (int i = 0; i < D2L; i++) {
+                    if (reBound(X, Y) != 0) {
+                        X = reBound(X, Y);
+                    }
+                    world[++X][Y] = Tileset.INSIDE;
+
+                    System.out.println("D2 Right-- X position:" + X);
+                }
+                if (initialY < Y) {
+                    for (int x = X; x >= initialX; x--) {
+                        for (int y = Y; y >= initialY; y--) {
+                            world[x][y] = Tileset.INSIDE;
+                        }
+                    }
+                }
+                if (initialY > Y) {
+                    for (int x = X; x >= initialX; x--) {
+                        for (int y = Y; y <= initialY; y++) {
+                            world[x][y] = Tileset.INSIDE;
+                        }
+                    }
+                }
+                break;
+            }
+
+            case "left" : {
+                for (int i = 0; i < D2L; i++) {
+                    if (reBound(X, Y) != 0) {
+                        X = reBound(X, Y);
+                    }
+                    world[--X][Y] = Tileset.INSIDE;
+
+                    System.out.println("D2 Left-- X position:" + X);
+                }
+                if (initialY < Y) {
+                    for (int x = X; x <= initialX; x++) {
+                        for (int y = Y; y >= initialY; y--) {
+                            world[x][y] = Tileset.INSIDE;
+                        }
+                    }
+                }
+                if (initialY > Y) {
+                    for (int x = X; x <= initialX; x++) {
+                        for (int y = Y; y <= initialY; y++) {
+                            world[x][y] = Tileset.INSIDE;
+                        }
+                    }
+                }
+                break;
+            }
+
+            case "up" : {
+                for (int i = 0; i < D2L; i++) {
+                    if (reBound(X, Y) != 0) {
+                        Y = reBound(X, Y);
+                    }
+                    world[X][++Y] = Tileset.INSIDE;
+
+                    System.out.println("D2 Up-- Y position:" + Y);
+                }
+                if (initialX < X) {
+                    for (int y = Y; y >= initialY; y--) {
+                        for (int x = X; x >= initialX; x--) {
+                            world[x][y] = Tileset.INSIDE;
+                        }
+                    }
+                }
+                if (initialX > X) {
+                    for (int y = Y; y >= initialY; y--) {
+                        for (int x = X; x <= initialX; x++) {
+                            world[x][y] = Tileset.INSIDE;
+                        }
+                    }
+                }
+                break;
+            }
+            case "down" : {
+                for (int i = 0; i < D2L; i++) {
+                    if (reBound(X, Y) != 0) {
+                        Y = reBound(X, Y);
+                    }
+                    world[X][--Y] = Tileset.INSIDE;
+
+                    System.out.println("D2 Down-- Y position:" + Y);
+                }
+                if (initialX < X) {
+                    for (int y = Y; y >= initialY; y--) {
+                        for (int x = X; x <= initialX; x++) {
+                            world[x][y] = Tileset.INSIDE;
+                        }
+                    }
+                }
+                if (initialX > X) {
+                    for (int y = Y; y >= initialY; y--) {
+                        for (int x = X; x >= initialX; x--) {
+                            world[x][y] = Tileset.INSIDE;
+                        }
+                    }
+                }
+            }
+            break;
+        }
+
+        return new int[] {X,Y};
+    }
+
+    private static int reBound(int X, int Y) {
+        if(X > w-2) {
+            for (int i = 0; i < 11; i++) {
+                world[X--][Y] = Tileset.INSIDE;
+            }
+            System.out.println("rebounded a high X");
+            return w - 2 - 11;
+        } else if (X < 2) {
+            for (int i = 0; i < 11; i++) {
+                world[X++][Y] = Tileset.INSIDE;
+            }
+            System.out.println("rebounded a low X");
+            return 2 + 4;
+        } else if (Y > h-2) {
+            for (int i = 0; i < 11; i++) {
+                world[X][Y--] = Tileset.INSIDE;
+            }
+            System.out.println("rebounded a high Y");
+            return h - 2 - 11;
+        } else if (Y < 2) {
+            for (int i = 0; i < 11; i++) {
+                world[X][++Y] = Tileset.INSIDE;
+            }
+
+            System.out.println("rebounded a low Y");
+            return 2 + 4;
+        }
+        return 0;
+    }
+}
+
+
