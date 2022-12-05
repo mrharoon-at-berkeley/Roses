4c11e8c2caf980dc03864240e89c7ae9ef16e59a
michael haroon <hmikehecht@gmail.com>
2022-12-04 18:54:18 -0800

    finished

--- proj3/byow/Core/Game.java
+++ proj3/byow/Core/Game.java
@@ -11,8 +11,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 public class Game {
@@ -65,7 +65,6 @@ public class Game {
 
 
     public TETile[][] startGame() {
-        //TODO?: When does game end?
         InputSource inputSource;
         if (render) {
             inputSource = new KeyboardInputSource();
@@ -176,10 +175,11 @@ public class Game {
         }
         StdDraw.textLeft(0, map.getHeight() - 2,
                 map.tileAt(x, y));
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        Date date = new Date();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
         StdDraw.textRight(map.getWidth(), map.getHeight() - 2,
-                formatter.format(date));
+                dtf.format(now));
         StdDraw.text(map.getWidth() / 2, map.getHeight() - 2,
                 "Number of Flowers to Collect: " + map.getNumberOfFlowers());
         StdDraw.show();
